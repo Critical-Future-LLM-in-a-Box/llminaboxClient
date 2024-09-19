bc3fb3d8b12ceaf153fb35d1cde37e75686aa8ef
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-09-19 21:55:17 +0300

    test

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "@/App";
 import { MaterialUIControllerProvider } from "@/context";
 
-const container = document.getElementById("app");
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
