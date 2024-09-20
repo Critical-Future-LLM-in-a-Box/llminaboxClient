1c23752b5afe0ce428c1658df81c566a333be8be
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-09-20 10:42:02 +0300

    wordpress deploy

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import App from "@/App";
 import { MaterialUIControllerProvider } from "@/context";
@@ -9,9 +9,9 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
+  <Router>
     <MaterialUIControllerProvider>
       <App />
     </MaterialUIControllerProvider>
-  </BrowserRouter>
+  </Router>
 );
